0359d08ecf799f0e675974e93c77fb59a27b18e5
michael mirhom <micharby73@gmail.com>
2023-08-22 03:13:57 -0500

    Try to solve syn error

--- digital_lib/database.py
+++ digital_lib/database.py
@@ -2,4 +2,5 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from digital_lib.models import Base
 DATABASE_CONFIG = ('sqlite', '///', 'digital_library.db')
-engine = create_engine(f'{DATABASE_CONFIG[0]}:{DATABASE_CONFIG[1]}{DATABASE_CONFIG[2]})
+engine = create_engine(f'{DATABASE_CONFIG[0]}:{DATABASE_CONFIG[1]}{DATABASE_CONFIG[2]}')
+                       
