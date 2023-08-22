19eb5a972bf84225aa5e98dc1a6ad306085ea0cb
michael mirhom <micharby73@gmail.com>
2023-08-22 14:37:50 -0500

    Start a new session

--- digital_lib/actions.py
+++ digital_lib/actions.py
@@ -2,3 +2,4 @@ from digital_lib.models import Author, Book, Genre
 from sqlalchemy.exc import SQLAlchemyError
 from digital_lib.database import Session
 def create_author(name):
+    session = Session()
