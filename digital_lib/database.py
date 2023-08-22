b253e2a853b42897097d88f56ba08ddadac3f114
michael mirhom <micharby73@gmail.com>
2023-08-22 03:15:00 -0500

    Create a session factory

--- digital_lib/database.py
+++ digital_lib/database.py
@@ -3,4 +3,5 @@ from sqlalchemy.orm import sessionmaker
 from digital_lib.models import Base
 DATABASE_CONFIG = ('sqlite', '///', 'digital_library.db')
 engine = create_engine(f'{DATABASE_CONFIG[0]}:{DATABASE_CONFIG[1]}{DATABASE_CONFIG[2]}')
+Session = sessionmaker(bind=engine)
                        
