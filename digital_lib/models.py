7a33de67a1a0b6c71f0f8c96e7437cf8547697c0
michael mirhom <micharby73@gmail.com>
2023-08-22 02:39:23 -0500

    Create Author database model

--- digital_lib/models.py
+++ digital_lib/models.py
@@ -1,3 +1,10 @@
 from sqlalchemy import Table, Column, Integer, String, ForeignKey, create_engine
 from sqlalchemy.orm import relationship, declarative_base
 Base = declarative_base()
+
+class Author(Base):
+    __tablename__ = 'authors'
+    
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
