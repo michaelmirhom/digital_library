83bcd74375b9092652a25d1bd96ed361befe1e97
michael mirhom <micharby73@gmail.com>
2023-08-22 14:41:34 -0500

    Check if the provided author name is empty

--- digital_lib/actions.py
+++ digital_lib/actions.py
@@ -3,3 +3,7 @@ from sqlalchemy.exc import SQLAlchemyError
 from digital_lib.database import Session
 def create_author(name):
     session = Session()
+    try:
+        if not name:
+            return "Author name cannot be empty!"
+        
