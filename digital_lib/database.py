160e80b2f6412857eae8db0379774fd0b7676abe
michael mirhom <micharby73@gmail.com>
2023-08-22 14:28:26 -0500

    "Update my Readme"

--- digital_lib/database.py
+++ digital_lib/database.py
@@ -4,6 +4,6 @@ from digital_lib.models import Base
 DATABASE_CONFIG = ('sqlite', '///', 'digital_library.db')
 engine = create_engine(f'{DATABASE_CONFIG[0]}:{DATABASE_CONFIG[1]}{DATABASE_CONFIG[2]}')
 Session = sessionmaker(bind=engine)
-    
+
 def init_db():
-    Base.metadata.create_all(engine)                   
+    Base.metadata.create_all(engine)                
