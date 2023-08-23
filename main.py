8368a2e714facc7219845cce6e44b828c37dad8b
michael mirhom <micharby73@gmail.com>
2023-08-23 17:25:46 -0500

    Display the Author Menu options to the user

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ import click
 from digital_lib.actions import create_author, create_book, list_books, list_authors, update_book, delete_book, search_books
 from digital_lib.database import init_db
 def author_menu():
+     while True:
+        click.echo("======== Author Menu ========")
+        click.echo("1. Add Author")
+        click.echo("2. List Authors")
+        click.echo("3. Go Back to Main Menu")
+        choice = click.prompt("Enter the number of your choice", type=str)
