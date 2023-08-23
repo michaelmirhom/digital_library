0ef59df88406125e4ba4cc1451fe69146cc1ed2e
michael mirhom <micharby73@gmail.com>
2023-08-23 17:35:31 -0500

    Solving synt errors

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import click
 from digital_lib.actions import create_author, create_book, list_books, list_authors, update_book, delete_book, search_books
 from digital_lib.database import init_db
+
+
 def author_menu():
-     choice = click.prompt("Enter the number of your choice", type=str)
-     while True:
+    while True:
         click.echo("======== Author Menu ========")
         click.echo("1. Add Author")
         click.echo("2. List Authors")
         click.echo("3. Go Back to Main Menu")
         choice = click.prompt("Enter the number of your choice", type=str)
+
         if choice == "1":
             name = click.prompt("Enter the name of the author")
             result = create_author(name)
@@ -16,8 +18,14 @@ def author_menu():
         elif choice == "2":
             authors = list_authors()
             for author in authors:
-                click.echo(author) 
+                click.echo(author)
         elif choice == "3":
             break
         else:
-            click.echo("Invalid choice, please try again.")      
+            click.echo("Invalid choice, please try again.")
+
+
+            
+
+
+
