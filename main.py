47e47bbd80a3958ad7512728158da95c9a958fb7
michael mirhom <micharby73@gmail.com>
2023-08-24 16:07:27 -0500

    Adding Break at line 60

--- main.py
+++ main.py
@@ -57,6 +57,7 @@ def book_menu():
             for book in books:
                 click.echo(f"{book['title']} by {book['author']}. Genres: {', '.join(book['genres'])}") 
         elif choice == "6":
+            break
 
         else:
             click.echo("Invalid choice, please try again.")    
